10081cdcc4b30778de0590e1c5546fcc0646fca6
mahmoud.bakhit@vodafone.com <mahmoud.bakhit@vodafone.com>
2021-12-21 20:17:00 +0200

    linting

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -5,9 +5,9 @@ import validator from '../../utilities/validator';
 const images = express.Router();
 
 images.get('/', validator, async (req, res) => {
-    const { filename, height, width } = req.query;
-    const image = await getResizedImage(filename as string,height as string,width as string);
-    return res.sendFile(image)
-  });
+  const { filename, height, width } = req.query;
+  const image = await getResizedImage(filename as string, height as string, width as string);
+  return res.sendFile(image);
+});
 
 export default images;
